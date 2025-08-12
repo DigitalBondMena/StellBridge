7cb04be81fc2a2f0e4b890c44e63f434f5a25bc3
marwanabubakr <marwanabubakr0o.o0@gmail.com>
2025-08-12 17:48:52 +0300

    make all edit

--- src/app/core/env.ts
+++ src/app/core/env.ts
@@ -1,2 +1,2 @@
 export const BASE_URL = 'https://steel.bondersmena.com/api/site/';
-export const IMAGE_BASE_URL = 'https://steel.bondersmena.com/';
+export const IMAGE_BASE_URL = 'https://bondersmena.com/steeldashboard/';
