4b576cc1be4d55738dfd76223e4338d15920b85f
marwanabubakr <marwanabubakr0o.o0@gmail.com>
2025-08-13 12:19:50 +0300

    slove hydration issue

--- src/app/pages/about/about.component.ts
+++ src/app/pages/about/about.component.ts
@@ -1,26 +1,30 @@
-import { IMAGE_BASE_URL } from './../../core/env';
 import { CommonModule } from '@angular/common';
 import { Component, inject, OnInit, signal } from '@angular/core';
+import { SafeHtmlPipe } from '../../core/pipes/safe-html.pipe';
 import { ClientsComponent } from '../home/components/clients/clients.component';
 import { LandingSectionComponent } from '../home/components/landing-section/landing-section.component';
+import { IMAGE_BASE_URL } from './../../core/env';
 import { IAbout } from './res/about';
 import { AboutService } from './res/about.service';
-import { SafeHtmlPipe } from '../../core/pipes/safe-html.pipe';
 @Component({
   selector: 'app-about',
   standalone: true,
-  imports: [CommonModule, ClientsComponent, LandingSectionComponent,SafeHtmlPipe],
+  imports: [
+    CommonModule,
+    ClientsComponent,
+    LandingSectionComponent,
+    SafeHtmlPipe,
+  ],
   templateUrl: './about.component.html',
   styleUrl: './about.component.css',
 })
 export class AboutComponent implements OnInit {
-  aboutData = signal<IAbout | null>(null);
+  aboutData = signal<IAbout>({} as IAbout);
   private aboutService = inject(AboutService);
-IMAGE_BASE_URL=IMAGE_BASE_URL;
+  IMAGE_BASE_URL = IMAGE_BASE_URL;
   ngOnInit(): void {
     this.aboutService.getAboutData().subscribe((data) => {
       this.aboutData.set(data);
-      console.log(this.aboutData());
     });
   }
 }
